76e8cba34a816f328fd2d164b28ab58d96fdfd9c
Paola Nicosia <paolanicosia9@gmail.com>
2022-04-02 23:17:14 +0200

    refactor

--- codility/js/easy/passingCars.js
+++ codility/js/easy/passingCars.js
@@ -9,15 +9,15 @@ function solution(A) {
   let pairCars = 0;
   let zeros = 0;
 
-  for(let car of A) {
-      if(car === 0) {
-          zeros++;
-      } else if(car === 1) {
-          pairCars = pairCars + zeros;
-      }
-      if(pairCars > 1000000000) {
-          return -1
-      }
+  for (let car of A) {
+    if (car === 0) {
+      zeros++;
+    } else if (car === 1) {
+      pairCars = pairCars + zeros;
+    }
+    if (pairCars > 1000000000) {
+      return -1
+    }
   }
   return pairCars;
 }
--- codility/js/easy/strSimmetryPoint.js
+++ codility/js/easy/strSimmetryPoint.js
@@ -7,21 +7,21 @@
 
 
 function solution(S) {
-  if(S.length === 1) {
-      return 0;
+  if (S.length === 1) {
+    return 0;
   }
-  if(S.length % 2 === 0) {
-      return -1;
+  if (S.length % 2 === 0) {
+    return -1;
   }
-  const centerX = Math.floor(S.length/2);
+  const centerX = Math.floor(S.length / 2);
   let indexDx = centerX + 1;
   let indexSx = centerX - 1;
-  for(let i=0; i<centerX; i++) {
-      if(S.charAt(indexDx) !== S.charAt(indexSx)) {
-          return -1;
-      }
-      indexDx++;
-      indexSx--;
+  for (let i = 0; i < centerX; i++) {
+    if (S.charAt(indexDx) !== S.charAt(indexSx)) {
+      return -1;
+    }
+    indexDx++;
+    indexSx--;
   }
   return centerX;
 }
